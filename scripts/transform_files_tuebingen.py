735de75d55bc8c6177d13854a2e3873b9dad76bb
jgciudad <jgciudad@outlook.com>
2024-03-15 11:58:55 +0100

    new data preprocessing and storing

--- scripts/transform_files_tuebingen.py
+++ scripts/transform_files_tuebingen.py
@@ -4,17 +4,20 @@ import argparse
 import sys
 import time
 from glob import glob
+import os
 from os.path import join, basename, isfile, realpath, dirname
 
 import h5py
 import numpy as np
 import tables
+import scipy.io
+import pandas as pd
 
 sys.path.insert(0, realpath(join(dirname(__file__), '..')))
 
 from base.data.downsample import downsample
 from base.config_loader import ConfigLoader
-from base.data.data_table import create_table_description, COLUMN_LABEL, COLUMN_MOUSE_ID
+from base.data.data_table import create_table_description, COLUMN_LABEL, COLUMN_MOUSE_ID, COLUMN_LAB
 
 
 def parse():
@@ -25,47 +28,47 @@ def parse():
     return parser.parse_args()
 
 
-def read_h5(h5_f_name: str):
-    """reads data from `h5_f_name`
+def read_recording(mat_path: str, sr_old: int, labs_channels: dict, lab_name: str):
+    rec = scipy.io.loadmat(mat_path)['signal']
 
-    Returns:
-        (dict, list, list): features in the form of a dict with entries for each CHANNEL, labels as a list, list of
-        start times of samples as indexes in features
-    """
-    features = {}  # save features per CHANNEL
-    # open h5 file in DATA_DIR
-    with h5py.File(join(config.DATA_DIR, h5_f_name), 'r') as h5_f:
-        # check if the sampling rates for all CHANNELS are the same, if not, throw an exception
-        sampling_rates = [h5_f[c].attrs['samprate'][0] for c in config.CHANNELS]
-        if np.all(sampling_rates == sampling_rates[0]):
-            sr = sampling_rates[0]
-        else:
-            raise ValueError('sampling rates in original h5 file are not equal:', sampling_rates)
+    features = {}
 
-        # iterate over CHANNELS, load and downsample data
-        for channel in config.CHANNELS:
-            data_c = h5_f[channel][:]
-            # convert the integer data (2 bytes (int16)) into doubles
-            scale_c = h5_f[channel].attrs['scale']
-            offset_c = h5_f[channel].attrs['offset']
-            features[channel] = np.double(data_c) * scale_c / 6553.6 + offset_c
+    for channel in labs_channels[lab_name].keys():
+        signal = rec[labs_channels[lab_name][channel]]
 
-            # downsample data to the SAMPLING_RATE specified in config
-            features[channel] = downsample(features[channel], sr_old=sr, sr_new=config.SAMPLING_RATE,
-                                           fmax=0.4 * config.SAMPLING_RATE, outtype='sos', method='pad')
+        s = downsample(signal,
+                       sr_old=sr_old,
+                       sr_new=config.SAMPLING_RATE,
+                       fmax=0.4 * config.SAMPLING_RATE,
+                       outtype='sos',
+                       method='pad')
+        
+        s = (s - np.mean(s, keepdims=True)) / np.std(s, keepdims=True)
 
-        # load scores from h5 file and map them to stages using SCORING_MAP
-        scores = h5_f['Scoring/scores'][:]
-        t_scoring_map = {v: k for k in config.SCORING_MAP for v in config.SCORING_MAP[k]}
-        labels = [t_scoring_map[s] if s in t_scoring_map else 'ignore' for s in scores]
-        # load start times of samples and transform them to indexes in the feature map
-        sample_start_times = h5_f['Scoring/times'][:]
-        sample_start_times = (sample_start_times / sr * config.SAMPLING_RATE).astype('int')
+        features[channel] = s
 
-    return features, labels, sample_start_times
+    sample_start_times = np.arange(0, features[channel].shape[0], config.SAMPLING_RATE*config.SAMPLE_DURATION, dtype=int)
+
+    return features, sample_start_times.tolist()
 
 
-def write_data_to_table(table: tables.Table, features: dict, labels: list, start_times: list, mouse_id: int):
+def read_labels(labels_path: str):
+    labels_correspondence = {'1': 'Wake',
+                            '2': 'Non REM',
+                            '3': 'REM',
+                            '4': 'Art'}
+        
+    if labels_path.split(os.sep)[-1][-3:] == 'mat':
+        labels = scipy.io.loadmat(labels_path)[os.path.basename(labels_path).split('.')[0]]
+    elif labels_path.split(os.sep)[-1][-3:] == 'csv':
+        labels = pd.read_csv(labels_path, index_col=0).iloc[:,0].to_numpy()
+
+    labels_str = [labels_correspondence[str(int(l))] for l in labels.squeeze()]
+
+    return labels_str
+
+
+def write_data_to_table(table: tables.Table, features: dict, labels: list, start_times: list, mouse_id: int, lab: str, labs_channels: dict):
     """writes given data to the passed table, each sample is written in a new row"""
     sample = table.row
 
@@ -77,13 +80,10 @@ def write_data_to_table(table: tables.Table, features: dict, labels: list, start
         # try to load data from sample_start to sample_end, if there is not enough data, ignore the sample
         try:
             sample[COLUMN_MOUSE_ID] = mouse_id
-            for c in config.CHANNELS:
+            sample[COLUMN_LAB] = lab
+            for c in labs_channels[lab].keys():
                 sample[c] = features[c][sample_start:sample_end]
-            # map stage from h5 file to stage used for classification
-            # if the mapping in STAGE_MAP is None, the sample is ignored
-            if label not in config.STAGE_MAP or config.STAGE_MAP[label] is None:
-                continue
-            sample[COLUMN_LABEL] = config.STAGE_MAP[label]
+            sample[COLUMN_LABEL] = label
             sample.append()
         except ValueError:
             print(f"""
@@ -93,18 +93,6 @@ def write_data_to_table(table: tables.Table, features: dict, labels: list, start
             """)
     # write data to table
     table.flush()
-
-
-def get_files_for_dataset(dataset: str):
-    """determines which files to transform for a given dataset, see DATA_SPLIT"""
-    # load all h5 files in DATA_DIR and sort them
-    files = [basename(fn) for fn in glob(join(config.DATA_DIR, '*.h5'))]
-    files.sort()
-
-    # search for files corresponding to mice name in DATA_SPLIT in files
-    file_list = [f for mouse_name in config.DATA_SPLIT[dataset] for f in files if mouse_name in f]
-
-    return file_list
 
 
 def transform():
@@ -122,30 +110,46 @@ def transform():
     print(f'data is loaded from {realpath(config.DATA_DIR)}')
 
     # open pytables DATA_FILE
-    with tables.open_file(config.DATA_FILE, mode='w', title='data from Tuebingen') as f:
+    with tables.open_file(config.DATA_FILE, mode='w', title='data from multiple labs') as f:
 
-        # datasets to create, if there is a dataset without any data, an empty table is created inside DATA_FILE
-        datasets = ['train', 'valid', 'test']
+        # create tables for every dataset
+        table = f.create_table(f.root, 'multiple_labs', table_desc, 'multiple_labs_data')
 
-        for dataset in datasets:
-            print('writing', dataset, 'data...')
+        # determine which files to transform for each dataset based on DATA_SPLIT
+        labs = [f for f in os.listdir(config.DATA_DIR) if '.' not in f]
+        # iterate over files, load them and write them to the created table
+        for l in labs:
+            lab_name = l.split('-')[0]
 
-            # create tables for every dataset
-            table = f.create_table(f.root, dataset, table_desc, dataset + ' data')
+            mice = [f for f in os.listdir(os.path.join(config.DATA_DIR, l)) if not f.startswith('.')]
 
-            # determine which files to transform for each dataset based on DATA_SPLIT
-            file_list = get_files_for_dataset(dataset)
-            # iterate over files, load them and write them to the created table
-            for i, f_name in enumerate(file_list):
-                print('mouse [{:d}/{:d}]: {:s}'.format(i + 1, len(file_list), f_name))
+            for m in mice:
+                try:
+                    assert os.path.isfile(os.path.join(config.DATA_DIR, l, m, 'cleaned', 'signal.mat'))
+                except:
+                    print(os.path.join(l, m), ' does not exist')
+                    continue
+
+                if os.path.isfile(os.path.join(config.DATA_DIR, l, m, 'cleaned', 'fs.mat')):
+                    original_fs = scipy.io.loadmat(os.path.join(config.DATA_DIR, l, m, 'cleaned', 'fs.mat'))['fs'][0][0]
+                else:
+                    original_fs = config.ORIGINAL_FS[lab_name]
+        
+                print('mouse {:s}'.format(os.path.join(l, m)))
                 start = time.time()
 
-                # load only numbers in mice name
-                mouse_id = int(''.join(filter(str.isdigit, f_name)))
-                # read h5 file, load and downsample data
-                features, labels, times = read_h5(f_name)
+                if lab_name == 'Kornum':
+                    labels_name = 'labels.csv'
+                elif lab_name == 'Maiken':
+                    labels_name = 'newSS.mat'
+                else:
+                    labels_name = 'labels.mat'
+
+                features, times = read_recording(os.path.join(config.DATA_DIR, l, m, 'cleaned', 'signal.mat'), original_fs, config.LABS_CHANNELS, lab_name)
+                labels = read_labels(os.path.join(config.DATA_DIR, l, m, 'cleaned', labels_name))
+
                 # write loaded data to table
-                write_data_to_table(table, features, labels, times, mouse_id)
+                write_data_to_table(table, features, labels, times, m, lab_name, config.LABS_CHANNELS)
 
                 print('execution time: {:.2f}'.format(time.time() - start))
                 print()
