5061b25b16d43d8b45d6814d078bf6a65fbfbedf
jgciudad <jgciudad@outlook.com>
2024-03-29 15:08:11 +0100

    adapted evaluation to multiple labs

--- scripts/evaluate_experiment_multiple_labs.py
+++ scripts/evaluate_experiment_multiple_labs.py
@@ -7,6 +7,9 @@ from os.path import basename, join, dirname, realpath, isfile
 import os
 import torch
 import torch.utils.data as t_data
+import openpyxl
+import sklearn.metrics
+import imblearn.metrics
 
 sys.path.insert(0, realpath(join(dirname(__file__), '..')))
 
@@ -25,11 +28,15 @@ def parse():
                         help="lab to test the model on: 'Antoine', 'Kornum', 'Alessandro', 'Sebastian' or 'Maiken'")
     parser.add_argument('--save_dir', '-s', required=True,
                         help="path to save model'")
+    parser.add_argument('--excel_path', '-ep', required=True,
+                        help="path to excel sheet to write metrics on'")
+    parser.add_argument('--row', '-r', required=True,
+                        help="row of frist lab to write in'")
 
     return parser.parse_args()
 
 
-def evaluation(test_lab):
+def evaluation(test_lab, r, excel_path):
     """evaluates best model in experiment on given dataset"""
     logger.fancy_log('start evaluation')
     result_logger = ResultLogger(config)
@@ -52,11 +59,33 @@ def evaluation(test_lab):
     labels, _ = evaluate(config, model, dataloader)
 
     # log/plot results
-    result_logger.log_sleep_stage_f1_scores(labels['actual'], labels['predicted'], args.dataset)
+    result_logger.log_sleep_stage_f1_scores(labels['actual'], labels['predicted'], test_lab)
     logger.logger.info('')
-    result_logger.log_confusion_matrix(labels['actual'], labels['predicted'], args.dataset, wo_plot=False)
-    result_logger.log_transformation_matrix(labels['actual'], labels['predicted'], args.dataset,
+    result_logger.log_confusion_matrix(labels['actual'], labels['predicted'], test_lab, wo_plot=False)
+    result_logger.log_transformation_matrix(labels['actual'], labels['predicted'], test_lab,
                                             wo_plot=False)
+    
+    # compute metrics
+    recall = sklearn.metrics.recall_score(y_true=labels['actual'], y_pred=labels['predicted'], average=None)
+    precision = sklearn.metrics.precision_score(y_true=labels['actual'], y_pred=labels['predicted'], average=None)
+    f1score = sklearn.metrics.f1_score(y_true=labels['actual'], y_pred=labels['predicted'], average=None)
+    specificity = imblearn.metrics.specificity_score(y_true=labels['actual'], y_pred=labels['predicted'], average=None)
+    acc = [sklearn.metrics.accuracy_score(y_true=labels['actual']==c, y_pred=labels['predicted']==c) for c in range(len(config.STAGES))]
+    bal_acc = (recall + specificity)/2
+    
+    wb = openpyxl.load_workbook(excel_path)   
+    sheet = wb["Sheet1"]
+    sheet.cell(row = r, column = 2).value = test_lab
+    for idx, c in enumerate(config.STAGES):
+        class_first_column = idx+3
+
+        sheet.cell(row = r, column = 3 + 5*idx).value = recall[idx]
+        sheet.cell(row = r, column = 3 + 5*idx + 1).value = precision[idx]
+        sheet.cell(row = r, column = 3 + 5*idx + 2).value = f1score[idx]
+        sheet.cell(row = r, column = 3 + 5*idx + 3).value = acc[idx]
+        sheet.cell(row = r, column = 3 + 5*idx + 4).value = bal_acc[idx]
+
+    wb.save(excel_path)
 
     logger.fancy_log('finished evaluation')
 
@@ -70,4 +99,4 @@ if __name__ == '__main__':
 
     logger.fancy_log('evaluate best model of experiment {} on dataset {}'.format(args.experiment, args.test_lab))
     # perform evaluation
-    evaluation(test_lab=args.test_lab)
+    evaluation(test_lab=args.test_lab, r=args.row, excel_path=args.excel_path)
